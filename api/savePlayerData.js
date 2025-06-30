f26a93d7c644e22446a99f69ee61882d23629822
diodg23 <101486538+diodg23@users.noreply.github.com>
2025-06-30 21:31:04 +0700

    Update savePlayerData.js

--- api/savePlayerData.js
+++ api/savePlayerData.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   // ✅ Tambahkan CORS Header di awal
   res.setHeader('Access-Control-Allow-Origin', 'https://cdn.dev.fun');
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-signature");
 
   // ✅ Tangani preflight OPTIONS
   if (req.method === 'OPTIONS') {
